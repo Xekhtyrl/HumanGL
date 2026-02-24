cb7bd5d8f5c2945be43cf1c3d61a3d16d1576047
lvodak <lvodak@student.42belgium.be>
2026-02-24 18:41:55 +0100

    basic function to reset the position of the model to paliate the translation of the animation

--- modelMatrices.cpp
+++ modelMatrices.cpp
@@ -7,7 +7,7 @@ extern unsigned int SCR_HEIGHT;
 // a simple function to reset the model matrix of each node of the model to identity and the scale to 1
 // and the translation to 0 (useful for reset and to set the base model matrix)
 // can be improved (maybe stop any animation and then also reset the rotation)
-void resetModelMatrix(IModel* object) {
+void resetNodesMatrix(IModel* object) {
 	HierarchicModel* modelPtr = dynamic_cast<HierarchicModel*>(object);
 	if (!modelPtr)
 		return;
@@ -46,7 +46,7 @@ void setBaseModelMatrix(GLFWwindow* window, IModel* object) {
 	// (void)normalizedCenter4; // avoid unused variable warning
 	center = vec3({normalizedCenter4[0], normalizedCenter4[1], normalizedCenter4[2]});
 	camera.resetCamera(window);
-	resetModelMatrix(object);
+	resetNodesMatrix(object);
 }
 
 /**
